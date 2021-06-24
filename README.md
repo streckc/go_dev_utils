4286b6c9e1025bd2a20b6d06c99cedf6174878db
Chad Streck <cstreck@tenable.com>
2021-06-24 16:44:43 -0500

    initial commit of first util

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# go_dev_utils
+# Go Development Utils
+
+Or development tools developed in Go.... what ever it is, I'm learning `go` and these are my playground projects.
+
+## test_mod
+
+A small utility to take monitor a list of files for modification time and run a command when a file in the list is modified.
+
+```
+$ test_mod "go run development.go" *.go
+$ test_mod "python3 develpment.py" development.py
+```
---
+++ src/test_mod.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const Usage = "test_mod <command> <file> [<file>...]"
+
+var filelist = make(map[string]time.Time)
+var command []string
+
+func init() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatal(Usage)
+	}
+	command = strings.Split(args[0], " ")
+
+	// prime the file list with zero'd times
+	for _, v := range args[1:] {
+		filelist[v] = time.Time{}
+	}
+}
+
+func timestamp() string {
+	return time.Now().Format("2006/01/02 15:04:05")
+}
+
+func getMaxFileTime() time.Time {
+	var maxTime time.Time
+
+	for _, t := range filelist {
+		if t.After(maxTime) {
+			maxTime = t
+		}
+	}
+
+	return maxTime
+}
+
+func updateFileModTimes() {
+	for k, _ := range filelist {
+		s, err := os.Stat(k)
+		for i := 3; i > 0 && err != nil; i-- {
+			time.Sleep(100 * time.Millisecond)
+			s, err = os.Stat(k)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		filelist[k] = s.ModTime()
+	}
+}
+
+func main() {
+	var lastTime time.Time
+	var currTime time.Time
+
+	for {
+		updateFileModTimes()
+		currTime = getMaxFileTime()
+		if lastTime.Before(currTime) {
+			fmt.Println(timestamp(), "Begin")
+			out, err := exec.Command(command[0], command[1:]...).Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(strings.TrimRight(string(out), "\n "))
+			fmt.Println(timestamp(), "End")
+		}
+		lastTime = currTime
+	}
+}
